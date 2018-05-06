955cdd8864c1282a08757e620c8e3e14835a8c45
Elior Abramovitz <elior.abramovitz@powtoon.com>
2018-05-06 12:02:22 +0300

    Add rotation to the hand animation

--- index.js
+++ index.js
@@ -3,28 +3,44 @@ const runTransition = () => {
   const CANVAS_HEIGHT = 576
   const HAND_IMAGE_WIDTH = 707
   const HAND_IMAGE_BEFORE_GRABBING_HEIGHT = 270
+  const HAND_ENTER_START_ANGLE = -30
+  const HAND_ENTER_END_ANGLE = 0
   const HAND_FIRST_POSITION_Y = 100
 
-  const canvas = document.getElementById("canvas")
-  canvas.width = CANVAS_WIDTH
-  canvas.height = CANVAS_HEIGHT
-  canvas.onclick = runTransition
+  const initTransition = () => {
+    const canvas = document.getElementById("canvas")
+    canvas.width = CANVAS_WIDTH
+    canvas.height = CANVAS_HEIGHT
+    canvas.onclick = runTransition
 
-  const ctx = canvas.getContext("2d")
+    const ctx = canvas.getContext("2d")
 
-  // Setting the image of hand before grabbing
-  const handSpriteImage = new Image()
-  handSpriteImage.src = "./images/hand-sprite.png"
-  handSpriteImage.xpos = CANVAS_WIDTH
-  handSpriteImage.ypos = CANVAS_HEIGHT
+    // Setting the image of hand before grabbing
+    const handSpriteImage = new Image()
+    handSpriteImage.src = "./images/hand-sprite.png"
+    handSpriteImage.xpos = CANVAS_WIDTH
+    handSpriteImage.ypos = CANVAS_HEIGHT
+    // handSpriteImage.xpos = CANVAS_WIDTH - HAND_IMAGE_WIDTH
+    // handSpriteImage.ypos = HAND_FIRST_POSITION_Y
 
-  handSpriteImage.onload = () => {
-    TweenMax.to(handSpriteImage, 0.7, {xpos: CANVAS_WIDTH - HAND_IMAGE_WIDTH, ypos: HAND_FIRST_POSITION_Y})
-    TweenLite.ticker.addEventListener("tick", animate)
+    // calculating hand enter angle
+    handSpriteImage.rotation = getRadianDegree(HAND_ENTER_START_ANGLE)
+
+    handSpriteImage.onload = () => {
+      const tl = new TimelineLite()
+      tl.to(handSpriteImage, 0.5, {xpos: CANVAS_WIDTH - HAND_IMAGE_WIDTH, ypos: HAND_FIRST_POSITION_Y, rotation: getRadianDegree(HAND_ENTER_END_ANGLE)})
+        // .to(handSpriteImage, 0.1, {opacity: 0})
+      TweenLite.ticker.addEventListener("tick", () => animate(ctx, handSpriteImage))
+    }
   }
 
-  const animate = () => {
+  const animate = (ctx, handSpriteImage) => {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
+    ctx.save()
+
+    ctx.translate(CANVAS_WIDTH, CANVAS_HEIGHT / 2)
+    ctx.rotate(handSpriteImage.rotation)
+    ctx.translate(-CANVAS_WIDTH, -(CANVAS_HEIGHT / 2))
 
     ctx.drawImage(
       handSpriteImage,
@@ -37,7 +53,15 @@ const runTransition = () => {
       HAND_IMAGE_WIDTH,
       HAND_IMAGE_BEFORE_GRABBING_HEIGHT
     )
+
+    ctx.restore()
   }
+
+  const getRadianDegree = (degree) => {
+    return degree * Math.PI / 180
+  }
+
+  initTransition()
 }
 
 runTransition()
